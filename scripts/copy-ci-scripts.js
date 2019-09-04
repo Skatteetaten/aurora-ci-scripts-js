c55849035e026eec455fa6311c384d54b6d0bdf0
Espen Strømland <espen.stromland@skatteetaten.no>
2019-09-04 09:55:57 +0200

    format

--- scripts/copy-ci-scripts.js
+++ scripts/copy-ci-scripts.js
@@ -1,6 +1,10 @@
-// Script for adding aurora-ci-scripts to node_modules, used as part of Jenkins pipeline
-const {execSync} = require("child_process")
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-execSync("chmod +x ./lib/bin/index.js")
-execSync("cp -r lib node_modules/aurora-ci-scripts")
-execSync("ln -s ../aurora-ci-scripts/bin/index.js node_modules/.bin/aurora-ci-scripts")
+// Script for adding aurora-ci-scripts to node_modules, used as part of Jenkins pipeline
+const { execSync } = require('child_process');
+
+execSync('chmod +x ./lib/bin/index.js');
+execSync('cp -r lib node_modules/aurora-ci-scripts');
+execSync(
+  'ln -s ../aurora-ci-scripts/bin/index.js node_modules/.bin/aurora-ci-scripts'
+);
